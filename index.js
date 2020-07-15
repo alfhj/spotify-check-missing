74d588b6c756db7e76700813bedd25bee35e8027
Alf Helge Jakobsen <alfern159@gmail.com>
2020-07-15 23:17:43 +0200

    Update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
-const promise = require('bluebird')
+// const promise = require('bluebird')
 const fs = require('fs')
 
-const token = 'BQDj5QhW--gfhqZJXybydoZ-XWU8wUW49PDb65EiygBm39-Q77kysw5EUuoUlasL4yGM2A3Snqh7MAsjfadwqIPZMrvK6P5zoN4rOTA9HsvfmPbTlLwK7VLZ0Q1Cp_3zr-kezpjOyvZg-0PGu_HACSauyJ4_AaM1isaYN2pARgQA5Gm-M08S1DxesvkiNfNWoW-0ZVpOfJxPo3dfvSnRj3JdRqL0gTczDe97SXTBu2WMT6-Ha24'
+const token = 'BQDh-Lqfnxkn7cG5K5GBDMP1uKcqPWjrRd6ME9sSMUYUw47cZeO1x0mVyxa2nibn_V3C5DR0Qzms_vfc4iRde42B9rLEyCYHyIIsJzdekmzMXfOCO-f9oaBpbGNw2xWMpQFG1f35Vux_b7i0U2YwZyeTWdZIppE3iCGbSJ8I_HBe73M4sqxCuqmr8ozehfKbO9-3ZkdiG0sRXrzFw4SrVhIaugiQfsOkMm0rZzzJ6OEIvVn-ltE'
 const urlBase = 'https://api.spotify.com/v1/me'
-const apiMaxItems = 50
+// const apiMaxItems = 50
 const apiHeaders = {
   Authorization: `Bearer ${token}`
 }
@@ -23,36 +23,57 @@ const printError = (error) => {
   throw new Error('Error')
 }
 
-const getTotal = async () => await axios.get(`${urlBase}/tracks`, {
+const getItemsPage = async (url) => await axios.get(url, {
   params: {
-    limit: 1
+    limit: 50
   },
   headers: apiHeaders
 })
-  .then((res) => res.data.total)
+  .then((res) => res.data)
   .catch((error) => printError(error))
 
-const getItemsPage = async (offset) => await axios.get(`${urlBase}/tracks`, {
-  params: {
-    limit: apiMaxItems,
-    offset
-  },
-  headers: apiHeaders
-})
-  .then((res) => res.data.items)
-  .catch((error) => printError(error))
+const getItems = async (url, log) => {
+  let nextUrl = url
+  const totalItems = []
+  while (nextUrl) {
+    const data = await getItemsPage(nextUrl)
+    log && console.log(`Got item ${data.offset + data.items.length} of ${data.total}`)
+    totalItems.push(...data.items)
+    nextUrl = data.next
+  }
+  return totalItems
+}
+
+// const compareSaved = (songs, albums)
+
+const readFileIfExists = (path) => {
+  if (fs.existsSync(path)) {
+    const data = fs.readFileSync(path)
+    return JSON.parse(data)
+  }
+}
 
 const run = async () => {
-  const total = await getTotal()
-  const songs = []
-  for (let i = 0; i < total; i += apiMaxItems) {
-    console.log(`Getting song ${i} of ${total}`)
-    const newSongs = await getItemsPage(i)
-    songs.push(...newSongs)
-  }
-  console.log(songs)
-  const data = JSON.stringify(songs)
-  fs.writeFile('songs.json', data, (err) => err && console.log(err))
+  console.log('Getting liked songs...')
+  const tracks = readFileIfExists('tracks.json') ||
+    (await getItems(`${urlBase}/tracks`, true)).map(t => t.track)
+
+  console.log('Getting albums...')
+  const partialAlbums = readFileIfExists('albums.json') ||
+    (await getItems(`${urlBase}/albums`, true)).map(a => a.album)
+
+  console.log('Getting missing album tracks...')
+  const albums = await Promise.all(partialAlbums.map(async a => {
+    const albumTracks = a.tracks && a.tracks.total > a.tracks.limit
+      ? await getItems(a.tracks.href, false)
+      : a.tracks.items
+    a.tracks = albumTracks
+    return a
+  }))
+
+  // const data = JSON.stringify(tracks)
+  // fs.writeFile('songs.json', data, (err) => err && console.log(err))
+  console.log('Done!')
 }
 
 run()
